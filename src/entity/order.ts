8e7888d40ff72067723ca818f1b8e74f8c64e769
Matheus Nobre <matheusnobre56@gmail.com>
2023-10-12 22:55:55 -0300

    added entities

---
+++ src/entity/order.ts
@@ -0,0 +1,17 @@
+import OrderItem from "./order_item";
+
+export default class Order {
+  _id: string;
+  _customerID: string;
+  _items: OrderItem[] = [];
+
+  constructor(id: string, customerID: string, items: OrderItem[]) {
+    this._id = id;
+    this._customerID = customerID;
+    this._items = items;
+  }
+
+  total(): number {
+    return this._items.reduce((acc, item) => acc + item._price, 0)
+  }
+}
