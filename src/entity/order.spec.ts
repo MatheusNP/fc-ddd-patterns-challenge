9f8131d2b7eabda36a5fa4e937360b2fcf894e9b
Matheus Nobre <matheusnobre56@gmail.com>
2023-10-22 20:41:36 -0300

    fix: adjusts in OrderItem price

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -22,11 +22,11 @@ describe("Order unit tests", () => {
   });
 
   it("should calculate total", () => {
-    const item1 =new OrderItem("i1", "Item 1", 100);
+    const item1 =new OrderItem("i1", "Item 1", 1, 100);
     const order1 = new Order("o1", "123", [item1])
     expect(order1.total()).toBe(100);
 
-    const item2 =new OrderItem("i2", "Item 2", 200);
+    const item2 =new OrderItem("i2", "Item 2", 1, 200);
     const order2 = new Order("o2", "123", [item1, item2])
     expect(order2.total()).toBe(300);
   });
--- src/entity/order_item.ts
+++ src/entity/order_item.ts
@@ -1,11 +1,17 @@
 export default class OrderItem {
   _id: string;
   _name: string;
+  _quantity: number;
   _price: number;
 
-  constructor(id: string, name: string, price: number) {
+  constructor(id: string, name: string, quantity: number, price: number) {
     this._id = id;
     this._name = name;
+    this._quantity = quantity;
     this._price = price;
+  }
+
+  orderItemTotal(): number {
+    return this._price * this._quantity;
   }
 }
